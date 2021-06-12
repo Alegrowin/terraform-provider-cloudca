d2646c21c8d7e559498b123262a42aee3b154350
Alexandre Proulx <aproulx@cloudops.com>
2021-06-11 21:48:44 -0400

    Do not track vendor folder

--- vendor/github.com/cloud-ca/go-cloudca/configuration/role.go
+++
@@ -1,9 +0,0 @@
-package configuration
-
-type Role struct {
-	Id           string       `json:"id,omitempty"`
-	Name         string       `json:"name,omitempty"`
-	Environment  Environment  `json:"environment,omitempty"`
-	Users        []User       `json:"users"`
-	Organization Organization `json:"organization,omitempty"`
-}
--- vendor/github.com/cloud-ca/go-cloudca/services/service.go
+++
@@ -1,6 +0,0 @@
-package services
-
-//Holds all the resources available for a particular service
-type ServiceResources interface {
-	GetServiceType() string
-}
